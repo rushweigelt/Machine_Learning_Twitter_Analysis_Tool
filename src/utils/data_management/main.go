c1b6f3bdcbd03b1e0a17f88b7e96314dff195ec8
Chris Bakopoulos <christian.bakopoulos@gmail.com>
2019-10-21 12:21:21 -0400

    Added TODOs for scaffolding

--- src/utils/data_management/main.go
+++ src/utils/data_management/main.go
@@ -1,16 +1,15 @@
 package data_management
 
 import (
-	"fmt",
 	"encoding/json"
+	"fmt"
 )
 
 // TODO: Get from a config or Docker Env var
-const API_SECRET = ""
-const API_URL_ROOT = ""
+const API_SECRET string = ""
+const API_URL_ROOT string = ""
 
 // TODO: Create routine for grabbing historical tweets by hashtag
 // TODO: Setup and teardown code for Mongo/Twitter API calls
 // TODO: Mongo Emission info
 // TODO: STRETCH routine for grabbing realtime data by hashtag
-
