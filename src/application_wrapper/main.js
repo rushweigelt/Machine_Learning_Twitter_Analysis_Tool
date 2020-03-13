f286f8c42b39422ba33217398505f2fb1096deca
rushw <39307793+rushweigelt@users.noreply.github.com>
2020-03-13 10:53:27 -0400

    Delete old, deprecated files in working django project

--- src/application_wrapper/main.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-let { app, BrowserWindow } = require('electron');
-
-app.on('ready', function() {
-    let mainWindow = new BrowserWindow();
-    mainWindow.loadURL('file://' + __dirname + '/app/index.html');
-});
